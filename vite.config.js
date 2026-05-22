e871d8d0d4aa235f73b1612303a690d4a54bee48
张帅 <47773565+254558@users.noreply.github.com>
2026-05-22 17:04:16 +0800

    hi

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
-import { cloudflare } from "@cloudflare/vite-plugin";
-
 export default defineConfig({
-  plugins: [vue(), cloudflare()],
+  plugins: [vue()], // 这里也去掉了 cloudflare()
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
